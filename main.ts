604db5b77f42d1325e71b00ccbfba4afc664c6f0
hritikabps <87896889+hritikabps@users.noreply.github.com>
2021-07-24 15:25:11 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+let Marks = 30
+if (Marks > 70) {
+    game.splash(" You got an A, Excellent")
+}
+
+if (Marks > 49 && 49 <= 70) {
+    game.splash(" You got a B, Good work")
+}
+
+if (Marks <= 49) {
+    game.splash(" You got a C, There is always scope for improvement")
+}
+
